b4ac1ec53bccd45622ccaa5e72c88bf7243d7f92
Martin Svarrer Christensen <fedlarm@gmail.com>
2013-11-12 22:34:29 +0100

    Updated slides to fit examples and talk. Buh jah!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 (function () {
 	Reveal.initialize({
-		history: true
+		history: true,
+		// Optional libraries used to extend on reveal.js
+		dependencies: [
+			{ src: 'libs/reveal/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+			{ src: 'libs/reveal/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: 'libs/reveal/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: 'libs/reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+			{ src: 'libs/reveal/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+			{ src: 'libs/reveal/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+		]
 	});
 })();
